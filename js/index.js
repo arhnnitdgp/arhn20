c6b4ed42e6b37a9810bb0c990f6906d2bdeda4c2
Kanishka Narjary <kanishkan14@gmail.com>
2020-01-17 03:31:41 +0530

    eye added

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// eye move with mouse
+var balls = document.getElementsByClassName("ball");
+document.onmousemove = function() {
+  var x = (event.clientX * 100) / window.innerWidth + "%";
+  var y = (event.clientY * 100) / window.innerHeight + "%";
+
+  balls[0].style.left = x;
+  balls[0].style.top = y;
+  balls[0].style.transform = "translate(-" + x + ",-" + y + ")";
+};
