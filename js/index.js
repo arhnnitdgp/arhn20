84ed60aec9980f0f9ef126434062971ad8d9279f
Kanishka Narjary <kanishkan14@gmail.com>
2020-01-17 19:30:31 +0530

    evnts anim

--- js/index.js
+++ js/index.js
@@ -9,7 +9,53 @@ document.onmousemove = function() {
   balls[0].style.transform = "translate(-" + x + ",-" + y + ")";
 };
 
-// scroll to section
-function goToByScroll(id) {
-  $("html,body").animate({ scrollTop: $("#" + id).offset().top }, "slow");
-}
+// events animation
+const animateBadge = () => {
+  TweenMax.to("#side1", 1, {
+    left: "-30px",
+    backgroundColor: "black",
+    opacity: 1,
+    ease: Power2.easeInOut
+  });
+  TweenMax.to("#side2", 1, {
+    left: "-30px",
+    backgroundColor: "black",
+    opacity: 1,
+    ease: Power2.easeInOut
+  });
+  TweenMax.to("#side3", 1, {
+    left: "-30px",
+    backgroundColor: "black",
+    opacity: 1,
+    ease: Power2.easeInOut
+  });
+  TweenMax.to("#side4", 1, {
+    left: "-30px",
+    backgroundColor: "black",
+    opacity: 1,
+    ease: Power2.easeInOut
+  });
+  TweenMax.to("#side5", 1, {
+    left: "-30px",
+    backgroundColor: "black",
+    opacity: 1,
+    ease: Power2.easeInOut
+  });
+  TweenMax.to("#side6", 1, {
+    left: "-30px",
+    backgroundColor: "black",
+    opacity: 1,
+    ease: Power2.easeInOut
+  });
+  console.log("anim1 done");
+};
+
+const rotateBadge = () => {
+  console.log("anim2 start");
+  TweenLite.defaultEase = Linear.easeNone;
+
+  var tl = new TimelineMax({ repeat: -1 });
+
+  //   tl.to(".rad", 5, { rotation: 360, transformOrigin: "right" });
+  //   tl.to(".sides", 5, { rotation: -360 }, 0);
+};
